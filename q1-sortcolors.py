e945c03467fe5bd493e1d67d7676f4df375ed741
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2021-07-23 00:00:30 +0530

    First commit

---
+++ q1-sortcolors.py
@@ -0,0 +1,18 @@
+def sortColors(nums):
+    l = mid = 0; u = len(nums) - 1
+    while mid <= u:
+        if nums[mid] == 0:
+            if nums[l] != 0:
+                nums[mid],nums[l] = nums[l],nums[mid]
+            else: mid += 1
+            l += 1
+            continue
+        if nums[mid] == 2:
+            if nums[u] != 2:
+                nums[mid],nums[u] = nums[u],nums[mid]
+            u -= 1
+            continue
+        if nums[mid] == 1: mid += 1
+    return nums
+nums = list(map(int,input().split()))
+print(sortColors(nums))
