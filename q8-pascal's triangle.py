d4c21b89f5fdbf9d796f80ae9b91d16ac91a1037
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2021-08-03 00:09:07 +0530

    added two variation problem

--- q8-pascal's triangle.py
+++ q8-pascal's triangle.py
@@ -17,10 +17,30 @@ print(generate(int(input())))
 # 1: Print the number at rth row and cth column:
 #    for that we can use the following formula:
 #        (r-1) C (c-1) in O(n)
+
+def generate(r,c):
+    prev = 1;
+    for i in range(1,c+1):
+        prev = (prev * (r - i + 1)) // i
+    return prev
+r,c = map(int,input().split())
+print(generate(r,c))
+
+
+
 # 2: Print the entire rth row:
 #    for that we can interate i through 0 to r-1,
 #    and then append  r C i which can be calculated in O(1) time using the
-#    formula rci = rC(i-1) * (r - i + i) / i
+#    formula rci = rC(i-1) * (r - i + 1) / i
 #    so the first element of the row will be rC0 that is 1 and using this
 #    mathematical formula we can easily find rest of the elements in total of O(r)
+
+def generate(r):
+    prev = 1;
+    ans = [1]
+    for i in range(1,r+1):
+        prev = (prev * (r - i + 1)) // i
+        ans.append(prev)
+    return ans
+print(*generate(int(input())))
 
