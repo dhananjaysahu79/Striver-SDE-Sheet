08c424ffbb588abf9d11ef5dc780b359e30a0bf0
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2021-11-15 21:44:01 +0530

    fc

--- q13-majority-element.py
+++ q13-majority-element.py
@@ -6,3 +6,35 @@ class Solution:
         for i in nums:
             dic[i] += 1
             if dic[i] > n/2: return i
+
+
+# Approach 2: O(1) space
+# O(NlogN) time
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        count = maxi = 1
+        max_num = nums[0]
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                count += 1
+            else: count = 1
+            if count > maxi:
+                max_num = nums[i]
+                maxi = count
+        return max_num
+
+# O(n) time and O(1) space
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        curr = nums[0]
+        count = 1
+        for i in nums[1:]:
+            if i == curr: count += 1
+            else: count -= 1
+            if not count:
+                curr = i
+                count = 1
+        return curr
